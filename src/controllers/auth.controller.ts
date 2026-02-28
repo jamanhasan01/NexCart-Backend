913de01e7aed6a2f8390fe825d27665542fed35c
jaman hasan <jamanhasan246@gmail.com>
2026-02-28 16:43:15 +0600

    update

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -60,7 +60,7 @@ export const loginUser = async (req: Request, res: Response, next: NextFunction)
     res.cookie('accessToken', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     res.status(200).json({ success: true, message: 'Login successfully' })
